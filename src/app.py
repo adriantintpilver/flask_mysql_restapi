905f4e9f1fcbaaa4b51c0264d2ffa071daa6ce03
adriantintpilver <adriantintpilver@gmail.com>
2023-01-02 17:36:10 -0300

    Update app.py

--- src/app.py
+++ src/app.py
@@ -1,10 +1,17 @@
 from flask import Flask, jsonify, request
 from flask_mysqldb import MySQL
 from flask_cors import CORS, cross_origin
+import ast
+from datetime import datetime
+from fastavro import writer, parse_schema
+from csv import reader
 import os
 import copy
 import json
-from datetime import datetime
+import pandas as pd
+import pandavro as pdx
+from avro.datafile import DataFileReader
+from avro.io import DatumReader
 
 from config import config
 from validations import *
@@ -67,6 +74,62 @@ def list_jobs():
     except Exception as ex:
         return jsonify({'message': "Error", 'success': False})
 
+# Function that returns an employee by id by DB
+def read_hired_employees_db(id):
+    try:
+        cursor = conexion.connection.cursor()
+        sql = "SELECT id, name, datetime, department_id, job_id FROM hired_employees WHERE id = '{0}'".format(id)
+        cursor.execute(sql)
+        data = cursor.fetchone()
+        if data != None:
+            hired_employee = {'id': data[0], 'name': data[1], 'datetime': data[2], 'department_id': data[3], 'job_id': data[4]}
+            return hired_employee
+        else:
+            return None
+    except Exception as ex:
+        raise ex
+
+# Function that returns an employee from id
+@app.route('/hired_employees/<id>', methods=['GET'])
+def read_hired_employees(id):
+    try:
+        hired_employee = read_hired_employees_db(id)
+        if hired_employee != None:
+            return jsonify({'id': hired_employee, 'message': "hired employee found.", 'success': True})
+        else:
+            return jsonify({'message': "hired employee not found.", 'success': False})
+    except Exception as ex:
+        return jsonify({'message': "Error", 'success': False})
+
+# Function that returns an department by department name from DB
+def read_department_db(department):
+    try:
+        cursor = conexion.connection.cursor()
+        sql = "SELECT id, department FROM departments WHERE department = '{0}'".format(department)
+        cursor.execute(sql)
+        data = cursor.fetchone()
+        if data != None:
+            department = {'id': data[0], 'department': data[1]}
+            return department
+        else:
+            return None
+    except Exception as ex:
+        raise ex    
+
+# Function that returns an job by job name from DB
+def read_jobs_db(job):
+    try:
+        cursor = conexion.connection.cursor()
+        sql = "SELECT id, job FROM jobs WHERE job = '{0}'".format(job)
+        cursor.execute(sql)
+        data = cursor.fetchone()
+        if data != None:
+            job = {'id': data[0], 'job': data[1]}
+            return job
+        else:
+            return None
+    except Exception as ex:
+        raise ex 
 
 def page_not_found(error):
     return "<h1>Page not found!, sorry d:-D </h1>", 404
