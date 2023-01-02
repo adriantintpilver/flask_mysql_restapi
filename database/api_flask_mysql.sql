91ea20dbf100fccce948c3673b4c97d53de96ab9
adriantintpilver <adriantintpilver@gmail.com>
2023-01-02 15:48:07 -0300

    Create api_flask_mysql.sql

---
+++ database/api_flask_mysql.sql
@@ -0,0 +1,57 @@
+-- Data base scripts
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "-03:00";
+--
+-- Base de datos: `globantdatastudy`
+--
+-- --------------------------------------------------------
+    CREATE DATABASE IF NOT EXISTS `globantdatastudy`;
+        
+    USE globantdatastudy;
+        --
+        -- Table departments script
+        --
+        CREATE TABLE IF NOT EXISTS `departments` (
+          `id` INT  AUTO_INCREMENT PRIMARY KEY NOT NULL,
+          `department` varchar(100) NOT NULL
+        ) ENGINE=InnoDB DEFAULT CHARSET=UTF8MB4 COMMENT='table for departments.';
+        --
+        -- Table jobs script
+        --
+        CREATE TABLE IF NOT EXISTS `jobs` (
+          `id` INT  AUTO_INCREMENT PRIMARY KEY NOT NULL,
+          `job` varchar(100) NOT NULL
+        ) ENGINE=InnoDB DEFAULT CHARSET=UTF8MB4 COMMENT='table for jobs.';
+        --
+        -- Table hired_employees script
+        --
+        CREATE TABLE `hired_employees` (
+          `id` int NOT NULL,
+          `name` varchar(100) NOT NULL,
+          `datetime` varchar(20) NOT NULL,
+          `department_id` int NOT NULL,
+          `job_id` int NOT NULL,
+          PRIMARY KEY (`id`),
+          KEY `fk_hired_employees_jobs` (`job_id`),
+          KEY `fk_hired_employees_departments` (`department_id`),
+          CONSTRAINT `fk_hired_employees_departments` FOREIGN KEY (`department_id`) REFERENCES `departments` (`id`),
+          CONSTRAINT `fk_hired_employees_jobs` FOREIGN KEY (`job_id`) REFERENCES `jobs` (`id`)
+        ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci COMMENT='table for hired employees.';
+        --
+        -- Stored procedure to truncate all tables in the backup restore feature
+        --
+        DROP procedure IF EXISTS `truncate_all`;
+        DELIMITER $$
+        USE `globantdatastudy`$$
+        CREATE PROCEDURE `truncate_all` ()
+        BEGIN
+          TRUNCATE TABLE globantdatastudy.hired_employees;
+          SET FOREIGN_KEY_CHECKS = 0;
+          TRUNCATE TABLE globantdatastudy.departments;
+          TRUNCATE TABLE globantdatastudy.jobs;
+          SET FOREIGN_KEY_CHECKS = 1;
+        END$$
+
+        DELIMITER ;
